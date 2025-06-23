317f184b79a7630621a54a44d0cf70ee870625b4
Ida Delphine <idadelm@gmail.com>
2025-06-23 23:43:42 +0100

    add generes

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,14 +1,26 @@
-import useData from "@/hooks/useData";
-import { type Genre } from "@/hooks/useGenres";
+import useGenres from "@/hooks/useGenres";
+import getCroppedImageUrl from "@/services/image-url";
+import { HStack, Image, List, Text } from "@chakra-ui/react";
 
 const GenreList = () => {
-  const { data } = useData<Genre>("/genres");
+  const { data } = useGenres();
   return (
-    <ul>
+    <List.Root>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <List.Item key={genre.id} listStyleType={"none"} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius="8px"
+              src={getCroppedImageUrl(genre.image_background)}
+              objectFit="cover"
+              alt={genre.name}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </List.Item>
       ))}
-    </ul>
+    </List.Root>
   );
 };
 
